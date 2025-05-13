25e140a39805c46507d75e2a96e5d885e495b767
AppliedAlpha <me@applied.kr>
2025-05-13 23:09:14 +0900

    📝 Implementing index.html & uploader for test

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, render_template
 from cv_model import model
-import os
+from PIL import Image, ImageDraw, ImageFont
+import io, os, base64
+import numpy as np
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
@@ -24,19 +26,53 @@ def predict():
 
     # Predict image
     results = model.predict(source=path, imgsz=640, conf=0.25)
+    box_data = results[0].cpu().boxes
 
-    res = results[0].cpu().numpy()
-    output = [
-        {
-        'class_id': int(det[5]),
-        'class_name': id2name[int(det[5])],
-        'confidence': float(det[4]),
-        'bbox': [float(det[0]), float(det[1]), float(det[2]), float(det[3])]
-        }
-        for det in res.boxes.data.tolist()
-    ]
+    # Open original image
+    img = Image.open(path).convert('RGB')
+    draw = ImageDraw.Draw(img)
+    try:
+        font = ImageFont.truetype("arial.ttf", size=16)
+    except:
+        font = ImageFont.load_default()
 
-    return jsonify({'predictions': output})
+    # Draw by boxes
+    xyxy = box_data.xyxy.cpu().numpy()   # [[x1,y1,x2,y2], ...]
+    confs = box_data.conf.cpu().numpy()  # [conf, ...]
+    cls_ids = box_data.cls.cpu().numpy() # [class_id, ...]
+
+    for (x1, y1, x2, y2), conf, cls in zip(xyxy, confs, cls_ids):
+        # Drawing Rectangle and label
+        draw.rectangle([x1, y1, x2, y2], outline="green", width=3)
+
+        label = f"{id2name[int(cls)]}:{conf:.4f}"
+        
+        x_min, y_min, x_max, y_max = font.getbbox(label)
+        text_width  = x_max - x_min
+        text_height = y_max - y_min
+
+        text_size = [text_width, text_height]
+        draw.rectangle([x1, y1 - text_size[1], x1 + text_size[0], y1], fill="green")
+        draw.text((x1, y1 - text_size[1]), label, fill="white", font=font)
+
+    # Image to Base64
+    buf = io.BytesIO()
+    img.save(buf, format='PNG')
+    img_b64 = base64.b64encode(buf.getvalue()).decode('ascii')
+
+    # JSON Serialize
+    return jsonify({
+        'predictions': [
+            {'class_id': int(c), 
+             'class_name': id2name[int(c)],
+             'confidence': float(f),
+             'bbox': [float(x1), float(y1), float(x2), float(y2)]
+            }
+            for (x1, y1, x2, y2), f, c in zip(xyxy, confs, cls_ids)
+        ],
+        'annotated_image': f"data:image/png;base64,{img_b64}"
+    })
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8882)
